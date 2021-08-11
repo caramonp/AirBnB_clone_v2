137970d09d2c8bb9e191f40d8f7552fa0af107d8
anamariaroman <2979@holbertonschool.com>
2021-08-10 22:54:29 -0500

    final changes for now in base model and more

--- models/base_model.py
+++ models/base_model.py
@@ -22,14 +22,16 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+        if kwargs:
+            for key_, value in kwargs:
+                if key_ != "__class__":
+                    setattr(self, key_, value)
+            if kwargs['updated_at']:
+                self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
+            if kwargs['created_at']:
+                self.created_at = datetime.strptime(kwargs['created_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -40,6 +42,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -50,4 +53,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary:
+            dictionary.pop ("_sa_instance_state")
         return dictionary
+
+    def delete(self):
+        from models import storage
+        storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models import city
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
-
+from models.city import City
 
 class State(BaseModel, Base):
     """ State class """
@@ -18,3 +19,9 @@ class State(BaseModel, Base):
     def cities(self):
         """ represent a relationship with the class City
         """
+        from models import  storage
+        list_cities = []
+        all_cities = storage.all(City)
+        for key, value in all_cities:
+            list_cities.append(value)
+        return list_cities
