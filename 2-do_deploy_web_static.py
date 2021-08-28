24632b02a03ab8d67914d9be8d1421394f8802d6
anamariaroman <77996965+anamariaroman@users.noreply.github.com>
2021-08-27 20:51:48 -0500

    task 2 and die slow

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Fabric script that distributes an archive to your web servers, using the
+function do_deploy """
+
+from fabric.api import env, put, run
+import os
+
+
+env.hosts = ['35.231.153.49', '100.26.59.175']
+
+
+def do_deploy(archive_path):
+    """ Distributes ans archive your web servers """
+    if os.path.exists(archive_path) is False:
+        return(False)
+    try:
+        put(archive_path, '/tmp/')
+        archive_name = archive_path.split('/')[-1]
+        archive_name2 = archive_name.split('.')[0]
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_name2))
+        run('tar zxf /tmp/{} -C /data/web_static/releases/{}'.format(
+            archive_name, archive_name2))
+        run('rm /tmp/{}'.format(archive_name))
+        run(
+            'mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/'.format(
+                archive_name2, archive_name2))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(
+            archive_name2))
+        run('rm -rf /data/web_static/current')
+        run('ln -s /data/web_static/releases/{} /data/web_static/current'.
+            format(archive_name2))
+        return True
+    except:
+        return False
